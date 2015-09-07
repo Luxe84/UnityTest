4dc5b06a86618ea1c30915a3692868ecd57d41cc
unknown <IROS-1@nb-569.dfki.net>
2015-09-07 17:18:32 +0200

    First unity project

---
+++ Assets/BuildWall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BuildWall : MonoBehaviour {
+	public Transform brick;
+
+	int width=5;
+	int height=5;
+
+	// Use this for initialization
+	void Start () {
+		for (int j = 0; j < height; j++)
+			for (int i = 0; i < width; i++)
+				Instantiate (brick, new Vector3 ((-width/2 + i) * brick.transform.localScale.x, 
+				                                 			 j  * brick.transform.localScale.x, 
+				                                 			 transform.position.z),
+				             Quaternion.identity);
+				                              
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
